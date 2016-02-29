47a9c9bd41d5293dd9f32da043379a98bdf9a7da
Dave Stanton <davestanton.us@gmail.com>
2016-02-29 14:51:55 -0500

    Now with multiplet templates.

--- bin/checklist.sh
+++ bin/checklist.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/gotoplanb/checklists/master/github/pull_request_template.md > pull_request_template.md
+case "$1" in
+pr)
+    curl https://raw.githubusercontent.com/gotoplanb/checklists/master/github/pull_request_template.md > pull_request_template.md
+    ;;
+ 
+issue)
+    curl https://raw.githubusercontent.com/gotoplanb/checklists/master/github/issue_template.md > issue_template.md
+    ;;
+*)
+    echo $"Usage: $0 {pr|issue}"
+    exit 1
+ 
+esac
